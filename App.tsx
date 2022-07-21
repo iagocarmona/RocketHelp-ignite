a1c23318caed596712d025f553eba74e420ae6ad
Iago Carmona <iagoortegacarmona@gmail.com>
2022-07-21 16:03:19 -0300

    :rocket: aula 3 finalizada, criando telas e routes.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import { NativeBaseProvider, StatusBar} from "native-base";
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import React from 'react'
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
-import {SignIn}  from './src/screens/SignIn'
-import {Loading} from './src/components/Loading'
+import { THEME } from './src/styles/theme'
 
-import {THEME} from "./src/styles/theme"
+import { Loading } from './src/components/Loading'
+
+import { Routes } from './src/routes'
 
 export default function App() {
-  const [fontLoaded] = useFonts({Roboto_700Bold, Roboto_400Regular});
+  const [fontLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
-      {fontLoaded ? <SignIn /> : <Loading />}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
-  );
+  )
 }
-
